474e8d0bbf0ad884ec03972bb44774983d02fa62
Valeria <83665852+itsvaleriaa@users.noreply.github.com>
2025-02-22 14:29:43 -0600

    CRUD categorias

--- Web.Ferreteria/Web/Controllers/CategoriesController.cs
+++ Web.Ferreteria/Web/Controllers/CategoriesController.cs
@@ -9,6 +9,7 @@ namespace Web.Controllers
     {
         private IConfiguracion _configuracion;
         private IList<Categories> categories { get; set; } = default!;
+        private Categories category { get; set; }
 
         public CategoriesController(IConfiguracion configuracion)
         {
@@ -23,17 +24,186 @@ namespace Web.Controllers
                 // Acepta cualquier certificado (solo en desarrollo)
                 ServerCertificateCustomValidationCallback = (sender, cert, chain, sslPolicyErrors) => true
             };
-            var cliente = new HttpClient(handler);
-            var solicitud = new HttpRequestMessage(HttpMethod.Get, endpoint);
-            var respuesta = await cliente.SendAsync(solicitud);
-            respuesta.EnsureSuccessStatusCode();
-            if (respuesta.StatusCode == System.Net.HttpStatusCode.OK)
+            var client = new HttpClient(handler);
+            var request = new HttpRequestMessage(HttpMethod.Get, endpoint);
+            var result = await client.SendAsync(request);
+            try
             {
-                var resultado = await respuesta.Content.ReadAsStringAsync();
-                var opciones = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
-                categories = JsonSerializer.Deserialize<List<Categories>>(resultado, opciones);
+                result.EnsureSuccessStatusCode();
+
+                if (result.StatusCode == System.Net.HttpStatusCode.OK)
+                {
+                    var requestResult = await result.Content.ReadAsStringAsync();
+
+                    try
+                    {
+                        var options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
+                        categories = JsonSerializer.Deserialize<List<Categories>>(requestResult, options);
+                    }
+                    catch (JsonException jsonEx)
+                    {
+                        Console.WriteLine($"Error de deserialización: {jsonEx.Message}");
+                    }
+                }
+            }
+            catch (HttpRequestException httpEx)
+            {
+                Console.WriteLine($"Error en la request HTTP: {httpEx.Message}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Se produjo un error inesperado: {ex.Message}");
             }
             return View(categories);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Categories category)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(category);
+            }
+
+            string endpoint = _configuracion.GetUrlMethod("ApiEndPoints", "AgregarCategorias");
+            var handler = new HttpClientHandler
+            {
+                ServerCertificateCustomValidationCallback = (sender, cert, chain, sslPolicyErrors) => true
+            };
+            var client = new HttpClient(handler);
+            try
+            {
+                var result = await client.PostAsJsonAsync(endpoint, category);
+                result.EnsureSuccessStatusCode();
+                if (result.StatusCode == System.Net.HttpStatusCode.OK)
+                {
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (HttpRequestException httpEx)
+            {
+                Console.WriteLine($"Error en la request HTTP: {httpEx.Message}");
+                return View(category);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Se produjo un error inesperado: {ex.Message}");
+                return View(category);
+            }
+            return View(category);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Edit(int Id)
+        {
+            string endpoint = _configuracion.GetUrlMethod("ApiEndPoints", "ObtenerCategoria");
+            var handler = new HttpClientHandler
+            {
+                ServerCertificateCustomValidationCallback = (sender, cert, chain, sslPolicyErrors) => true
+            };
+            var client = new HttpClient(handler);
+            var request = new HttpRequestMessage(HttpMethod.Get, string.Format(endpoint, Id));
+            var result = await client.SendAsync(request);
+            try
+            {
+                result.EnsureSuccessStatusCode();
+
+                if (result.StatusCode == System.Net.HttpStatusCode.OK)
+                {
+                    var requestResult = await result.Content.ReadAsStringAsync();
+
+                    try
+                    {
+                        var options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
+                        category = JsonSerializer.Deserialize<Categories>(requestResult, options);
+                    }
+                    catch (JsonException jsonEx)
+                    {
+                        Console.WriteLine($"Error de deserialización: {jsonEx.Message}");
+                    }
+                }
+            }
+            catch (HttpRequestException httpEx)
+            {
+                Console.WriteLine($"Error en la request HTTP: {httpEx.Message}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Se produjo un error inesperado: {ex.Message}");
+            }
+            return View(category);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(Categories category)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(category);
+            }
+
+            string endpoint = _configuracion.GetUrlMethod("ApiEndPoints", "EditarCategorias");
+            var handler = new HttpClientHandler
+            {
+                ServerCertificateCustomValidationCallback = (sender, cert, chain, sslPolicyErrors) => true
+            };
+            var client = new HttpClient(handler);
+            try
+            {
+                var result = await client.PutAsJsonAsync(endpoint, category);
+                result.EnsureSuccessStatusCode();
+                if (result.StatusCode == System.Net.HttpStatusCode.NoContent)
+                {
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (HttpRequestException httpEx)
+            {
+                Console.WriteLine($"Error en la request HTTP: {httpEx.Message}");
+                return View(category);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Se produjo un error inesperado: {ex.Message}");
+                return View(category);
+            }
+            return View(category);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(int Id)
+        {
+            string endpoint = _configuracion.GetUrlMethod("ApiEndPoints", "EliminarCategorias");
+            var handler = new HttpClientHandler
+            {
+                ServerCertificateCustomValidationCallback = (sender, cert, chain, sslPolicyErrors) => true
+            };
+            var client = new HttpClient(handler);
+            var request = new HttpRequestMessage(HttpMethod.Delete, string.Format(endpoint, Id));
+            var result = await client.SendAsync(request);
+            try
+            {
+                result.EnsureSuccessStatusCode();
+
+                if (result.StatusCode == System.Net.HttpStatusCode.NoContent)
+                {
+                    return RedirectToAction("Index", "Categories");
+                }
+            }
+            catch (HttpRequestException httpEx)
+            {
+                Console.WriteLine($"Error en la request HTTP: {httpEx.Message}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Se produjo un error inesperado: {ex.Message}");
+            }
+            return RedirectToAction("Index", "Categories");
         }
     }
 }
